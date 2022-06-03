c6d955f1115da7f26315968564ff2d50a34d597e
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-06-03 16:25:23 +0430

    make a show image meme with memePage.js🎉

---
+++ js/memepage.js
@@ -0,0 +1,19 @@
+const memeId = new URLSearchParams(window.location.search).get('meme')
+const imgShowMeme = document.querySelector("img")
+const containerElement = document.querySelector(".container")
+if(isNaN(memeId))
+{
+    containerElement.style.display = "flex";
+    containerElement.style.justifyContent= "center";
+    containerElement.style.alignItems = "center";
+
+    containerElement.innerHTML = `<h1>opss, not found a meme plz go to <a href="../"> home page</a>  </h1>`
+}else if(Number(memeId)>37)
+{
+    containerElement.style.display = "flex";
+    containerElement.style.justifyContent= "center";
+    containerElement.style.alignItems = "center";
+
+    containerElement.innerHTML = `<h1>opss, not found a meme plz go to <a href="../"> home page</a>  </h1>`
+}
+imgShowMeme.src=`meme/${memeId}.jpg`
